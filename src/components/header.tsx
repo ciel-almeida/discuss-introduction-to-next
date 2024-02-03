f36475d7a172edddd535e5268e2d52c8b2dfd06f
Cleverton Almeida <clevertoncas@gmail.com>
2024-02-03 19:08:41 -0300

    add top posts feature in homepage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Input } from '@nextui-org/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react';
 import HeaderAuth from './header-auth';
+import SearchInput from './search-input';
 
 export default function Header() {
 	return (
@@ -12,7 +13,7 @@ export default function Header() {
 			</NavbarBrand>
 			<NavbarContent justify="center">
 				<NavbarItem>
-					<Input />{' '}
+					<SearchInput />
 				</NavbarItem>
 			</NavbarContent>
 			<NavbarContent justify="end">
