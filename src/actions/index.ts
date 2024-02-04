9f4fb4b3e01d4fc1abe2560c9aa1fadcddde32c5
Cleverton Almeida <clevertoncas@gmail.com>
2024-02-03 21:45:39 -0300

    add search feature

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,4 @@ export { signOut } from './signOut';
 export { createPost } from './create-post';
 export { createTopic } from './create-topic';
 export { createComment } from './create-comment';
+export { search } from './search';
